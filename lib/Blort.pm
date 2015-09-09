76b5988d65297aa630119a02ccacc459a956ec67
Shawn Laffan <shawnlaffan@gmail.com>
2015-09-09 16:28:25 +1000

    Add a makefile and dummy lib

---
+++ lib/Blort.pm
@@ -0,0 +1,20 @@
+package Module;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw();
+
+our $VERSION = 1;
+
+1;
+
+
+=head1 NAME
+
+Blort - Move along, nothing to see here.
+
+=head1 VERSION
+
+Version 1
