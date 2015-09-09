de826ca90dff0adb2f1d9777dcfacd3616a77a09
Shawn Laffan <shawnlaffan@gmail.com>
2015-09-09 20:05:54 +1000

    More fiddle

--- install_gdal.sh
+++ install_gdal.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # install gdal
 
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-apt-get update -qq
-apt-get install libgdal1h libgdal-dev
+#add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+#apt-get update -qq
+#apt-get install libgdal1h libgdal-dev
 #apt-get install -y python-gdal python3-gdal
+
+
+wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz
+tar xvz gdal-1.11.1.tar.gz
+cd gdal-1.11.1
+
+./configure --with-python --with-perl --prefix=~/bin
+make
+make install
