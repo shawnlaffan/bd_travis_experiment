6bd7c5f22f53d8cb7423b438a627a11b4980c3b9
Shawn Laffan <shawnlaffan@gmail.com>
2017-01-31 21:18:28 +1100

    Add comment to clarify argument

--- strawberry_notes/build_gdal.sh
+++ strawberry_notes/build_gdal.sh
@@ -4,6 +4,7 @@ export PATH=${ST_PERL}/c/bin:${ST_PERL}/perl/bin:${GDAL_ROOT}:${PATH}
 export LIBRARY_PATH=${ST_PERL}/perl/lib/CORE:${ST_PERL}/c/lib:${GDAL_ROOT}
 export CPATH=${ST_PERL}/c/include:${ST_PERL}/perl/lib/CORE:${GDAL_ROOT}/port:${GDAL_ROOT}/gcore:${GDAL_ROOT}/ogr:${GDAL_ROOT}/alg:${GDAL_ROOT}/apps
 cd ${GDAL_ROOT}
+#  curl causes issues in the Geo::GDAL build - prob just needs extra library paths to be added above
 ./configure --with-curl=no
 make -j4
 cd -
