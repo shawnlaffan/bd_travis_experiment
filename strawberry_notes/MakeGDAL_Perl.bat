6557dff263ebec4960790cef5161985941d541a0
Shawn Laffan <shawnlaffan@gmail.com>
2017-01-31 20:03:40 +1100

    Add shiny new notes

--- strawberry_notes/MakeGDAL_Perl.bat
+++ strawberry_notes/MakeGDAL_Perl.bat
@@ -1,18 +1,28 @@
-#  steps courtesy Anderson Ku
 rem Make Geo::GDAL Perl Binding
-set PERL_ROOT=D:\sperl
-set GDAL_ROOT=D:\gdal
-set MSYS_ROOT=D:\msys64
+rem  steps courtesy Anderson Ku
+rem Update base paths to match your system
+
+set PERL_ROOT=C:\berrybrew\5.24.0_64_PDL
+set GDAL_ROOT=C:\gdal_builds\gdal-2.1.2
+set MSYS_ROOT=C:\msys64
 set PATH=%PERL_ROOT%\c\bin;%PERL_ROOT%\perl\bin;%MSYS_ROOT%\usr\bin;%PATH%
-set CPATH=%PERL_ROOT%\c\include;%PERL_ROOT%\perl\lib\CORE;%GDAL_ROOT%\port;%GDAL_ROOT%\gcore;%GDAL_ROOT%\ogr;%GDAL_ROOT%\alg;%GDAL_ROOT%\apps;
+set CPATH=%PERL_ROOT%\c\include;%PERL_ROOT%\perl\lib\CORE;%GDAL_ROOT%\port;%GDAL_ROOT%\gcore;%GDAL_ROOT%\ogr;%GDAL_ROOT%\alg;%GDAL_ROOT%\apps;%PERL_ROOT%\c\x86_64-w64-mingw32\include\c++;
 
 call make clean
-perl Makefile.PL --gdal-config=%GDAL_ROOT%/apps/gdal-config --no-version-check --gdal-source-tree=%GDAL_ROOT%
+perl Makefile.PL MAKE=gmake --gdal-config=%GDAL_ROOT%/apps/gdal-config --no-version-check --gdal-source-tree=%GDAL_ROOT%
 
-#SWL: should perlify these
-sed -i -e 's^|^^#include ^<iostream^>^|^/^/#include ^<iostream^>^|' *wrap.cpp
-sed -i -e 's^|^^void VSIStdoutUnsetRedirection()^|#undef fwrite\nvoid VSIStdoutUnsetRedirection()^|' *wrap.cpp
+:: Redundant now we add include\c++ to CPATH
+:: SWL: should perlify these
+:: sed -i -e 's^|^^#include ^<iostream^>^|^/^/#include ^<iostream^>^|' *wrap.cpp
+:: sed -i -e 's^|^^void VSIStdoutUnsetRedirection()^|#undef fwrite\nvoid VSIStdoutUnsetRedirection()^|' *wrap.cpp
+
+:: Redundant when using gmake
+::  definitely need to perlify these, as the third one does not run under windows:
+:: sed -i -e 's/RANLIB = rem/RANLIB = ranlib/' Makefile_*
+:: sed -i -e 's/NOOP = rem/NOOP =/' Makefile_*
+:: sed -i -e 's|DIRFILESEP = \\|DIRFILESEP = /|' Makefile_*
 
 call make
-call make install
-call prove
+call make test
+:: call make install
+:: call prove
---
+++ strawberry_notes/build_gdal.sh
@@ -0,0 +1,9 @@
+export ST_PERL=/c/berrybrew/5.24.0_64_PDL
+export GDAL_ROOT=/c/gdal_builds/gdal-2.1.2
+export PATH=${ST_PERL}/c/bin:${ST_PERL}/perl/bin:${GDAL_ROOT}:${PATH}
+export LIBRARY_PATH=${ST_PERL}/perl/lib/CORE:${ST_PERL}/c/lib:${GDAL_ROOT}
+export CPATH=${ST_PERL}/c/include:${ST_PERL}/perl/lib/CORE:${GDAL_ROOT}/port:${GDAL_ROOT}/gcore:${GDAL_ROOT}/ogr:${GDAL_ROOT}/alg:${GDAL_ROOT}/apps
+cd ${GDAL_ROOT}
+./configure --with-curl=no
+make -j4
+cd -
